0c94c925e3bf63c4422ac76fdb329294d61198e3
Qianchuan Ye <yeqianchuan@gmail.com>
2022-10-03 14:06:57 -0400

    Need to make sure both parties conceal in sync

--- test/millionaire.ml
+++ test/millionaire.ml
@@ -4,14 +4,12 @@ let run party n =
   setup_driver "127.0.0.1" 12345 party;
   let a =
     if party == party_alice
-    then obliv_array_concat (private_s_int n) (private_s_int 0)
-    (* else obliv_array_new_from 2 party_alice in *)
-    else obliv_array_concat (s_int_from n party_alice) (s_int_from 0 party_alice) in
+    then private_s_int n
+    else obliv_array_new_from 1 party_alice in
   let b =
     if party == party_bob
-    then obliv_array_concat (private_s_int n) (private_s_int 0)
-    (* else obliv_array_new_from 2 party_bob in *)
-    else obliv_array_concat (s_int_from n party_bob) (s_int_from 0 party_bob) in
+    then private_s_int n
+    else obliv_array_new_from 1 party_bob in
   let c = obliv_int_le a b in
   if unsafe_r_bool c
   then print_endline "bob"
